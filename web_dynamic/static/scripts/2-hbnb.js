63a6bdd939fccf4772a0c5554b06305ff2c54311
rChrisb <5767@holbertonstudents.com>
2023-04-24 18:40:42 +0200

    change color depending of API status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,12 +1,12 @@
 const $ = window.$;
 
 $(document).ready(function () {
-  $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
+  $.get("http://127.0.0.1:5001/api/v1/status/", function (data) {
     if (data.status === "OK") {
-      $("#api_status").addClass("available");
-    } else {
+      $("#api_status").css("background-color", "#ff545f");
+    } /* else {
       $("#api_status").removeClass("available");
-    }
+    } */
   });
 
   $('input[type="checkbox"]').change(function () {
